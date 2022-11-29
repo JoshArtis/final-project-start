cbbefbeafb132de77e4c42071888a95b6341e51f
Matt Whalen <mwhalen@udel.edu>
2022-11-29 00:57:25 -0500

    Fixed prettier issue

--- src/Plate.tsx
+++ src/Plate.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-extra-parens */
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { DropTargetMonitor, useDrop } from "react-dnd";
 import { ItemTypes } from "./constants";
